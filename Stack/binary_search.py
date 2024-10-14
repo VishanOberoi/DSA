4f1debbe7bd976ddbcb741d739381be7b105c027
VishanOberoi <vishanoberoi14@gmail.com>
2024-10-14 16:13:16 -0400

    binary search

--- Stack/binary_search.py
+++ Stack/binary_search.py
@@ -3,6 +3,7 @@ class Solution:
         left, right = 0, len(target) - 1
         while left <= right:
             mid = (right + left) // 2
+            #mid = right + (left - right)//2 prevents overflow error in other languages.
             if nums[mid] == target:
                 return mid
             elif nums[mid] > target:
