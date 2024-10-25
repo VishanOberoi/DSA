61e02ede64ebd0602d56cdfd93040494dd7992ed
VishanOberoi <vishanoberoi14@gmail.com>
2024-10-25 05:28:26 -0400

    bst

---
+++ Trees/kth_smallest_iterative.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def kthSmallest(self, root, k):
+        n = 0
+        stack = []
+        curr = root
+
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            
+            curr = stack.pop()
+            n += 1
+            if n == k:
+                return curr.val
+            
+            curr = curr.right
+
+
+
+            
+            
+
+            
